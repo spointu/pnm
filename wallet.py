a866fc83e88308be3dfd2ae8b16191447f261179
SPO <sp@tegami.io>
2025-01-21 07:38:28 +0100

    Comment wallet.py code

--- wallet.py
+++ wallet.py
@@ -3,10 +3,13 @@ class Wallet:
         self.balance = balance
 
     def set_balance(self, val):
+        # Add val to balance
         self.balance = self.balance + val
 
     def get_balance(self):
+        # Get balance
         return self.balance
 
     def remove_balance(self, val):
+        # Remove val from balance
         self.balance = self.balance - val
